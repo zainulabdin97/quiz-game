65b6352e5b38da6ece9ed2d0d7ee62d6dfe08dbf
Zain Ul Abdin <44570560+zainulabdin97@users.noreply.github.com>
2024-03-09 23:49:58 +0100

    Add files via upload

---
+++ quiz_brain.py
@@ -0,0 +1,28 @@
+class QuizBrain:
+
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+    def next_question(self):
+        user_answer = input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False)?: ")
+        self.correct_answer(user_answer, self.question_list[self.question_number].answer)
+        self.question_number += 1
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
+
+    def correct_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+
+        else:
+            print("You got it wrong!")
+
+        print(f"The correct answer is: {correct_answer}")
+
+
+
